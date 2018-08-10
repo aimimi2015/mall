1da8544c8365572de61ac716c9a972d85effd8d5
aimimi2015-mac <804323873@qq.com>
2018-08-11 02:58:17 +0800

    1

---
+++ src/main/java/com/mmall/controller/intercept/Interceptor.java
@@ -0,0 +1,92 @@
+//package com.mmall.controller.intercept;
+//
+//import com.mmall.common.Const;
+//import com.mmall.common.ResponseCode;
+//import com.mmall.common.ServerResponse;
+//import com.mmall.pojo.User;
+//import net.sf.json.JSONObject;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.util.List;
+//
+///**
+// * session拦截器
+// * Created by ${aimimi2015} on 2017/8/24.
+// */
+//@Controller
+//public class Interceptor implements HandlerInterceptor{
+//
+//    //    进入handle方法之前
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//
+//        User user=(User)request.getSession().getAttribute(Const.CURRENT_USER);
+//        if (user != null) {
+//
+//            return true;
+//        }
+////        if (request.getHeader("x-requested-with") != null) {
+//
+//            /*
+//            * 这个地方不知道怎么处理ajax请求
+//            * */
+//            /* 设置格式为text/json */
+//
+//            response.setContentType("text/json");
+//
+//		/* 设置字符集为'UTF-8' */
+//
+//        response.setCharacterEncoding("UTF-8");
+//        try {
+//
+//            PrintWriter write = response.getWriter();
+//            response.reset();
+//
+//            String jsonstr = "{\"tatus\":\"0\",\"msg\":\"buguo\"}";
+//            JSONObject json_test = JSONObject.fromObject(jsonstr);
+//
+//            write.write(json_test.toString());
+//
+//            write.flush();
+//
+//            write.close();
+//
+//        } catch (IOException e) {
+//
+//            e.printStackTrace();
+//
+//        }
+//
+//
+////        else {
+//////
+////            request.getRequestDispatcher("/user/relogin").forward(request, response);
+////        }
+//
+//        return false;
+//
+//
+//    }
+//
+//
+//
+//
+//    //进入方法之后
+//    @Override
+//    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+//
+//    }
+//
+//    @Override
+//    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+//
+//    }
+//}
+//
---
+++ target/mmall/index.jsp
@@ -0,0 +1,50 @@
+<%@ page language="java"  contentType="text/html; charset=UTF-8" %>
+<%
+    String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+    request.setAttribute("basePath", basePath);
+
+%>
+
+
+<html>
+<body>
+
+${basePath}
+
+
+<form name="input" action="${basePath}/user/login" method="post">
+    <table cellpadding="0" cellspacing="5" border="0">
+        <tr>
+            <th>账号：</th>
+            <td><input type="text" name="username"></td>
+        </tr>
+        <tr>
+            <th>密码：</th>
+            <td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+            <th></th>
+            <td>
+                <input type="submit" value="确定">
+                <input type="reset" value="取消">
+            </td>
+        </tr>
+    </table>
+</form>
+
+
+springmvc上传文件
+<form name="form1" action="${basePath}/manage/product/upload.do" method="post" enctype="multipart/form-data">
+    <input type="file" name="upload_file" />
+    <input type="submit" value="springmvc上传文件" />
+</form>
+
+
+富文本图片上传文件
+<form name="form2" action="${basePath}/manage/product/richtext_img_upload.do" method="post" enctype="multipart/form-data">
+    <input type="file" name="upload_file" />
+    <input type="submit" value="富文本图片上传文件" />
+</form>
+
+</body>
+</html>
